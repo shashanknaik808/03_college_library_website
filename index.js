16a6d81317b60ad9fd28d679b0d32fc163761507
Shashank Naik <shashanknaik808@gmail.com>
2023-04-17 03:56:32 +0530

    Added methods to display prototype

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ function Display() {
 }
 
 // Add methods to display prototype
+Display.prototype.add = function (book) {
+    console.log("Adding to UI");
+}
+
+Display.prototype.clear = function () {
+    let libraryForm = document.getElementById('libraryForm');
+    libraryForm.reset();
+}
 
 // Add submit event listener to libraryForm
 let libraryForm = document.getElementById('libraryForm');
@@ -21,4 +29,27 @@ libraryForm.addEventListener('submit', libraryFormSubmit);
 function libraryFormSubmit(e) {
     e.preventDefault();
     console.log('You have submitted library form');
+    let name = document.getElementById('bookName').value;
+    let author = document.getElementById('author').value;
+    let type;
+    let fiction = document.getElementById('fiction');
+    let programming = document.getElementById('programming');
+    let cooking = document.getElementById('cooking');
+
+    if (fiction.checked) {
+        type = fiction.value;
+    }
+    else if (programming.checked) {
+        type = programming.value;
+    }
+    else if (cooking.checked) {
+        type = cooking.value;
+    }
+
+    let book = new Book(name, author, type);
+    console.log(book);
+
+    let display = new Display();
+    display.add(book);
+    display.clear();
 }
