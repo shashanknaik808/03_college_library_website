edbbb560abfaba2529e02232a7ee720455917f0e
Shashank Naik <shashanknaik808@gmail.com>
2023-04-17 03:34:05 +0530

    Added submit event listener to libraryForm

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("This is index.js");
+
+// Constructor
+function Book(name, author, type) {
+    this.name = name;
+    this.author = author;
+    this.type = type;
+}
+
+// Display Constructor
+function Display() {
+
+}
+
+// Add methods to display prototype
+
+// Add submit event listener to libraryForm
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+    e.preventDefault();
+    console.log('You have submitted library form');
+}
